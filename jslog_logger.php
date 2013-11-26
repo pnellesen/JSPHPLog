f15b033d59e6ace81200d354a3496a180c212459
pnellesen <pnellesen@hotmail.com>
2013-11-26 12:02:54 -0600

    Initial commit of base logger script. Very much alpha

---
+++ jslog_logger.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Created on Nov 25, 2013 by Pat Nellesen (pat@pnellesen.com)
+ *
+ * Simple logger to write Javascript error messages to system Error log file.
+ * Expects JSNLog (http://js.jsnlog.com/) to be implemented in web page code.
+ * 
+ */
+ $jslog_input = json_decode($HTTP_RAW_POST_DATA,true);
+ var_dump($jslog_input);
+ $jslog_msg = $jslog_input["lg"][0]["m"];
+ $jslog_timestamp = $jslog_input["lg"][0]["t"];
+ //error_log("[JSPHPLog]: $jslog_msg", 0);
+?>
