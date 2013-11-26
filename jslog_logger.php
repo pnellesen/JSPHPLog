2c1da3d6120b3762ca50e2dfa17c864428dd2e9f
pnellesen <pnellesen@hotmail.com>
2013-11-26 15:39:58 -0600

    Added check for lg array element in the request data.

--- jslog_logger.php
+++ jslog_logger.php
@@ -7,8 +7,13 @@
  * 
  */
  $jslog_input = json_decode($HTTP_RAW_POST_DATA,true);
- $jslog_msg = $jslog_input["lg"][0]["m"];
- $jslog_timestamp = $jslog_input["lg"][0]["t"];
- error_log("[JSPHPLog]: $jslog_msg", 0);
- echo(json_encode(array('log_status','success')));
+ if (array_key_exists("lg",$jslog_input)) {
+	 $jslog_msg = $jslog_input["lg"][0]["m"];
+	 $jslog_timestamp = $jslog_input["lg"][0]["t"];
+	 error_log("[JSPHPLog]: $jslog_msg", 0);
+	 $msg = "success";	
+ } else {
+ 	$msg = "failure - no [lg] element found in request";
+ }
+ echo(json_encode(array('result',$msg)));// return a success/fail message;
 ?>
