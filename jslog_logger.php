f29ec663c37d45bd04fb9895b7eb4bb6b594a8fc
pnellesen <pnellesen@hotmail.com>
2013-11-27 09:29:06 -0600

    Update jslog_logger.php

--- jslog_logger.php
+++ jslog_logger.php
@@ -8,9 +8,10 @@
  */
  $jslog_input = json_decode($HTTP_RAW_POST_DATA,true);
  if (array_key_exists("lg",$jslog_input)) {
-	 $jslog_msg = $jslog_input["lg"][0]["m"];
-	 $jslog_timestamp = $jslog_input["lg"][0]["t"];
-	 error_log("[JSPHPLog]: $jslog_msg", 0);
+ 	 foreach($jslog_input["lg"] as $msgItem) {
+	 	 $jslog_msg = $msgItem["m"];
+		 error_log("[JSPHPLog]: $jslog_msg", 0);
+ 	 }
 	 $msg = "success";	
  } else {
  	$msg = "failure - no [lg] element found in request";
