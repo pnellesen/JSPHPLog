c3c97b4b30cf463d83aa5e77f6034bfa4587bd68
pnellesen <pnellesen@hotmail.com>
2013-11-27 23:24:39 -0600

    Updated to use Apache2 $_SERVER variable if set in

--- jslog_config.php
+++ jslog_config.php
@@ -7,21 +7,25 @@
  * Include this script on any page/site you wish
  * implement JSNLog on. Needs to be before the JSNLog loggers are initialized
  */
- $jsnlog_opts = Array(
- 	"src" => "/lib/jsnlog.min.js",// Location of the jsnlog Javascript file,
- 	"apndOpts" => Array (
- 		"url" => "jslog_logger.php",// URL to send the logs to
- 		"batchSize" => 3
- 	)
- );
+ if (isset($_SERVER["JSLogConfig"])) {
+	$JSLogConfig = json_decode($_SERVER["JSLogConfig"],true);// User server ENV variable if found;
+ } else {
+ 	$JSLogConfig = Array(
+	 	"src" => "/lib/jsnlog.min.js",// Location of the jsnlog Javascript file,
+	 	"appenderOpts" => Array (
+	 		"url" => "jslog_logger.php",// URL to send the logs to
+	 		"batchSize" => 3
+	   )
+	);
+ }
  ?>
- <script type="text/javascript" src="<?php echo($jsnlog_opts["src"]);?>"></script>
+ <script type="text/javascript" src="<?php echo($JSLogConfig["src"]);?>"></script>
  <script language="javascript">
   	var appender = JL.createAjaxAppender();
   	<?php
   	$js_output = "";
-  	if (count($jsnlog_opts["apndOpts"]) > 0) {
-  		$js_output = "appender.setOptions(".json_encode($jsnlog_opts["apndOpts"]).");\n";
+  	if (count($JSLogConfig["appenderOpts"]) > 0) {
+  		$js_output = "appender.setOptions(".json_encode($JSLogConfig["appenderOpts"]).");\n";
   	}
   	echo($js_output);
   	?>
