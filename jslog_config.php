aa8e182c1319f8ce0aef965598f57d55cbf3b9d9
pnellesen <pnellesen@hotmail.com>
2013-11-30 10:52:00 -0600

    Updated to handle "default" configuration.

--- jslog_config.php
+++ jslog_config.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="/lib/jsnlog.min.js"></script>
 <?php
 /*
  * Created on Nov 25, 2013
@@ -7,25 +8,27 @@
  * Include this script on any page/site you wish
  * implement JSNLog on. Needs to be before the JSNLog loggers are initialized
  */
+ $JLAppenderCfg = "";
+ $JLLoggerCfg = "";
+ $js_output = "";
  if (isset($_SERVER["JLAppenderCfg"])) {
 	$JLAppenderCfg = json_decode($_SERVER["JLAppenderCfg"],true);// User server ENV variable if found;
  }
  if (isset($_SERVER["JLLoggerCfg"])) {
  	$JLLoggerCfg = json_decode($_SERVER["JLLoggerCfg"],true);// User server ENV variable if found;
  }
- ?>
- <script type="text/javascript" src="/lib/jsnlog.min.js"></script>
- <script language="javascript">
-  	var appender = JL.createAjaxAppender();
-  	<?php
-  	$js_output = "";
-  	if (isset($JLAppenderCfg) && count($JLAppenderCfg["appenderOpts"]) > 0) {
-  		$js_output = "appender.setOptions(".json_encode($JLAppenderCfg["appenderOpts"]).");\n";
-  	}
-  	echo($js_output);
-  	?>
-  	JL().setOptions({
-  		"appenders": [appender]
-  	});
- </script>
- 
+ if ($JLAppenderCfg != "" || $JLLoggerCfg != "") {
+ 	$js_output = "<script language=\"javascript\">\n";
+ 	if ($JLAppenderCfg != "") {
+ 		$js_output .= "var appender = JL.createAjaxAppender();\n";
+ 		if (count($JLAppenderCfg) > 0) {
+  			$js_output .= "appender.setOptions(".json_encode($JLAppenderCfg).");\n";
+  		}	
+ 		$js_output .= "JL().setOptions({
+  				\"appenders\": [appender]
+  			});\n";
+ 	}
+ 	$js_output .= "</script>\n";
+ 	echo($js_output);
+ }
+ ?> 
