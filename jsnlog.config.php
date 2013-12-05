ca48190daf472147c83e67487776637b622202bd
pnellesen <pnellesen@hotmail.com>
2013-12-05 14:31:31 -0600

    Updates to allow named logger configuration and Global logger options to be set via .ini file.

--- jsnlog.config.php
+++ jsnlog.config.php
@@ -12,25 +12,43 @@ $JLKeys = parse_ini_file("jsnlog.ini.php",true);
 $JLCfg = array_values($JLKeys);
 $appender_ids = array();
 $js_output = "";
+$appender_output = "";
+$logger_output = "";
+$JL_output = "";
 
 if (is_array($JLCfg) && count($JLCfg) > 0) {
 	$js_output = "<script language=\"javascript\">\n";		
 	foreach(array_keys($JLKeys) as $key => $item) {
 	 	if (array_key_exists('type',$JLCfg[$key]) and $JLCfg[$key]['type'] == "appender") {
-		 	$js_output .= "var " . $item . "= JL.createAjaxAppender();\n";
+	 		// Set up Appenders
+		 	$appender_output .= "var " . $item . "= JL.createAjaxAppender();\n";
 		 	if (array_key_exists('options',$JLCfg[$key]) && count($JLCfg[$key]['options']) > 0) {
-		 		$js_output .= $item . ".setOptions(".json_encode($JLCfg[$key]['options']).");\n";
+		 		$appender_output .= $item . ".setOptions(".json_encode($JLCfg[$key]['options']).");\n";
 		 	}
 		 	array_push($appender_ids,$item);
 	 	} elseif (array_key_exists('type',$JLCfg[$key]) and $JLCfg[$key]['type'] == "logger") {
-	 		//echo ("Got a logger: " . $item . "\n");
+	 		// Set up Loggers
+	 		$logger_output .= "var " . $item . "=JL('" . $item. "');\n";
+	 		if (array_key_exists('options',$JLCfg[$key]) && count($JLCfg[$key]['options']) > 0) {
+	 			$logger_output .= $item . ".setOptions(".json_encode($JLCfg[$key]['options']).");\n";
+	 		} elseif (array_key_exists('appenders',$JLCfg[$key]) && $JLCfg[$key]['appenders'] != '') {
+	 			$logger_output .= $item . ".setOptions({\"appenders\":[" .$JLCfg[$key]['appenders'] . "]});\n";
+	 		}
+	 	} elseif (array_key_exists('type',$JLCfg[$key]) and $JLCfg[$key]['type'] == "JL" && array_key_exists('options',$JLCfg[$key]) && count($JLCfg[$key]['options']) > 0) {
+	 		$JL_output .= $item . ".setOptions(".json_encode($JLCfg[$key]['options']).");\n";
 	 	}
  	}	
+	$js_output .= $appender_output;
+	
+	if ($logger_output != "") {// named logger configs found
+		$js_output .= $logger_output;
+	} else {// use default Logger
+		$js_output .= "JL().setOptions({\"appenders\": [" . implode(",",$appender_ids) . "]});\n";	
+	}
+	
+	$js_output .= $JL_output;
+	$js_output .= "</script>\n";
 }
-$js_output .= "JL().setOptions({
-		\"appenders\": [" . implode(",",$appender_ids) . "]
-	});\n";
-$js_output .= "</script>\n";
+
 echo($js_output);
-//print_r($JLCfg);
 ?> 
--- jsnlog.ini.php
+++ jsnlog.ini.php
@@ -4,21 +4,43 @@
 ; see JSNLog (http://js.jsnlog.com/) documentation for additional information
 
 [appender_1]
-type="appender" ;type field is required. Can be "appender" or "logger"
-options["url"]="jsnlog.logger.php"
+type="appender" ;type field is required. Can be "appender","logger","JL", or "server"
+options[url]="jsnlog.logger.php"
 
-; add additional JSNLog Appender options as shown below
-; options["batchSize"]=2
-; options["level"]=2000
+;add additional JSNLog Appender options as shown below (Default values indicated)
+;options[level]="TRACE"
+;options[userAgentRegex]=''
+;options[ipRegex]=''
+;options[disallow]=''
+;options[storeInBufferLevel]="ALL"
+;options[sendWithBufferLevel]="OFF"
+;options[bufferSize]=0
+;options[batchSize]=1
 
-;[appender_2] ; can add additional appenders as desired.
+;You can add additional appenders as desired.
+;[appender_2] 
 ;type="appender"
 ;options[url]="jsnlog.logger.php"
 ;options[batchSize]=3
 ;options[level]=2000
  
-
-;[logger_1] ; can also add additional loggers and logger options as needed
+; You can also add additional loggers and logger options. Note that the Logger calls in your Javascript must reference the correct logger if named loggers are used.
+;[logger_1] 
 ;type="logger"
+;appenders=appender_1,appender_2 ; Used to tell loggers which appenders to use.
+;options[level]=2000
+;options[userAgentRegex]=''
+;options[ipRegex]=''
+;options[disallow]=''
+;options[onceOnly]=''
 
-;options['level']=2000
+;Set Global options for all Loggers
+;[JL]
+;type="JL"
+;options[type]="enabled"
+;options[clientIP]=""
+;options[requestId]=""
+
+;Set options to be used in jsnlog.logger.php. Things like whether or not to send emails, etc.
+;[server]
+;type="server"
