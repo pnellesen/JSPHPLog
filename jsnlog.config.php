46dc47f2c329f4ff52ad1e4add3901105c332c5a
pnellesen <pnellesen@hotmail.com>
2013-12-04 18:09:06 -0600

    Changes to use .ini file more efficiently

--- jsnlog.config.php
+++ jsnlog.config.php
@@ -8,23 +8,29 @@
  * Include this script on any page/site you wish
  * implement JSNLog on. Needs to be before the JSNLog loggers are initialized
  */
+$JLKeys = parse_ini_file("jsnlog.ini.php",true);
+$JLCfg = array_values($JLKeys);
+$appender_ids = array();
+$js_output = "";
 
- $JLAppenderCfg = parse_ini_file("jsnlog.ini.php");
- $JLLoggerCfg = "";
- $js_output = "";
-
- if ($JLAppenderCfg != "" || $JLLoggerCfg != "") {
- 	$js_output = "<script language=\"javascript\">\n";
- 	if ($JLAppenderCfg != "") {
- 		$js_output .= "var appender = JL.createAjaxAppender();\n";
- 		if (count($JLAppenderCfg) > 0) {
-  			$js_output .= "appender.setOptions(".json_encode($JLAppenderCfg).");\n";
-  		}	
- 		$js_output .= "JL().setOptions({
-  				\"appenders\": [appender]
-  			});\n";
- 	}
- 	$js_output .= "</script>\n";
- 	echo($js_output);
- }
- ?> 
+if (is_array($JLCfg) && count($JLCfg) > 0) {
+	$js_output = "<script language=\"javascript\">\n";		
+	foreach(array_keys($JLKeys) as $key => $item) {
+	 	if (array_key_exists('type',$JLCfg[$key]) and $JLCfg[$key]['type'] == "appender") {
+		 	$js_output .= "var " . $item . "= JL.createAjaxAppender();\n";
+		 	if (array_key_exists('options',$JLCfg[$key]) && count($JLCfg[$key]['options']) > 0) {
+		 		$js_output .= $item . ".setOptions(".json_encode($JLCfg[$key]['options']).");\n";
+		 	}
+		 	array_push($appender_ids,$item);
+	 	} elseif (array_key_exists('type',$JLCfg[$key]) and $JLCfg[$key]['type'] == "logger") {
+	 		//echo ("Got a logger: " . $item . "\n");
+	 	}
+ 	}	
+}
+$js_output .= "JL().setOptions({
+		\"appenders\": [" . implode(",",$appender_ids) . "]
+	});\n";
+$js_output .= "</script>\n";
+echo($js_output);
+//print_r($JLCfg);
+?> 
--- jsnlog.ini.php
+++ jsnlog.ini.php
@@ -3,12 +3,22 @@
 ; Configuration file for JSPHPLog. Will set up default logger/appenders and their options
 ; see JSNLog (http://js.jsnlog.com/) documentation for additional information
 
-[appender]
-url="jsnlog.logger.php"
+[appender_1]
+type="appender" ;type field is required. Can be "appender" or "logger"
+options["url"]="jsnlog.logger.php"
 
-; add other appender options as well
-; batchSize=2
-; level=2000
+; add additional JSNLog Appender options as shown below
+; options["batchSize"]=2
+; options["level"]=2000
 
-;[logger]
-; add logger options here if needed
+;[appender_2] ; can add additional appenders as desired.
+;type="appender"
+;options[url]="jsnlog.logger.php"
+;options[batchSize]=3
+;options[level]=2000
+ 
+
+;[logger_1] ; can also add additional loggers and logger options as needed
+;type="logger"
+
+;options['level']=2000
