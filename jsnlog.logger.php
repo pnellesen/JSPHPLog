1cdfff627db6b3093ff7b64c57738dd0cc087533
pnellesen <pnellesen@hotmail.com>
2013-12-16 14:36:55 -0600

    Slight cleanup/refactoring.

--- jsnlog.logger.php
+++ jsnlog.logger.php
@@ -6,33 +6,32 @@
  * Expects JSNLog (http://js.jsnlog.com/) to be implemented in web page code.
  *
  */
- $JLKeys = parse_ini_file("jsnlog.ini.php",true);
- $msg_type = 0;
- $destination = "";
- $extra_headers = "";
- if (is_array($JLKeys) && array_key_exists('server',$JLKeys)) {
- 	foreach($JLKeys['server'] as $key => $item) {
- 		switch($key) {
- 			case "msg_type":
- 				$msg_type = $item;
- 				break;
- 			case "destination":
- 				$destination = $item;
- 				break;
- 			case "extra_headers":
- 				$extra_headers = $item;
- 		}
-  	}
- }
- $jslog_input = json_decode($HTTP_RAW_POST_DATA,true);
- if (is_array($jslog_input) && array_key_exists("lg",$jslog_input)) {
- 	 foreach($jslog_input["lg"] as $msgItem) {
+$msg = "failure - no JSNLog messages found in request";
+$jslog_input = json_decode($HTTP_RAW_POST_DATA,true);
+if (is_array($jslog_input) && array_key_exists("lg",$jslog_input)) {
+	$msg="success";
+	 $JLKeys = parse_ini_file("jsnlog.ini.php",true);
+	 $log_options = array(0,'','');
+	 if (is_array($JLKeys) && array_key_exists('server',$JLKeys)) {
+	 	foreach($JLKeys['server'] as $key => $item) {
+	 		switch($key) {
+	 			case "msg_type":
+	 				$log_options[0] = $item;
+	 				break;
+	 			case "destination":
+	 				$log_options[1] = $item;
+	 				break;
+	 			case "extra_headers":
+	 				$log_options[2] = $item;
+	 				break;
+	 		}
+	  	}
+	 }
+		 foreach($jslog_input["lg"] as $msgItem) {
 	 	 $jslog_msg = $msgItem["m"];
-		 error_log("[JSPHPLog]: $jslog_msg", $msg_type, $destination, $extra_headers);
- 	 }
-	 $msg = "success";	
- } else {
- 	$msg = "failure - no [lg] element found in request";
- }
- echo(json_encode(array('result',$msg)));// return a success/fail message;
+		 $log_result = error_log("[JSPHPLog]: $jslog_msg" . "\n", $log_options[0], $log_options[1], $log_options[2]);
+	 }
+	 if (!$log_result) $msg="failure - problem writing to error_log";	
+}
+echo(json_encode(array('result',$msg)));// return a success/fail message;
 ?>
